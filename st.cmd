9455dc1919a07cdec6737a1038df3032b890afa6
Carlos Doro Neto <carlos.neto@ess.eu>
2026-05-25 16:31:05 +0200

    Add axis config

--- st.cmd
+++ st.cmd
@@ -7,7 +7,8 @@ require ecmccfg 8.0.0
 iocshLoad "$(ecmccfg_DIR)/startup.cmd" "ECMC_VER=8.0.2, NAMING=ESSnaming"
 
 iocshLoad "$(ecmccfg_DIR)/addSlave.cmd" "HW_DESC=EK1100, DEFAULT_SUBS=false"
-...
+iocshLoad "$(ecmccfg_DIR)/addSlave.cmd" "HW_DESC=EL1808, DEFAULT_SUBS=false"
+iocshLoad "$(ecmccfg_DIR)/addSlave.cmd" "HW_DESC=EL7041-0052, DEFAULT_SUBS=false"
 
 iocshLoad "$(ecmccfg_DIR)/configureAxis.cmd" "CONFIG=$(E3_CMD_TOP)/cfg/axis1.ax"
 
