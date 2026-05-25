fa7a3c2d739cebbf73c8fdd01e02b86cbaf1e3b8
Carlos Doro Neto <carlos.neto@ess.eu>
2026-05-25 11:38:31 +0200

    Create project structure

---
+++ README.md
@@ -0,0 +1,1 @@
+# 2026 EPICS Summer School
---
+++ st.cmd
@@ -0,0 +1,15 @@
+#!/usr/bin/env iocsh
+
+epicsEnvSet IOC "NeverGonna:GiveYouUp"
+
+require ecmccfg 8.0.0
+
+iocshLoad "$(ecmccfg_DIR)/startup.cmd" "ECMC_VER=8.0.2, NAMING=ESSnaming"
+
+iocshLoad "$(ecmccfg_DIR)/addSlave.cmd" "HW_DESC=EK1100, DEFAULT_SUBS=false"
+...
+
+iocshLoad "$(ecmccfg_DIR)/configureAxis.cmd" "CONFIG=$(E3_CMD_TOP)/cfg/axis1.ax"
+
+iocshLoad "$(ecmccfg_DIR)/applyConfig.cmd"
+iocshLoad "$(ecmccfg_DIR)/setAppMode.cmd"
